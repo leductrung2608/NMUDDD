c9136fa0c2d45c38639610c8faca40f2ee61f226
leductrung2310 <19522422@gm.uit.edu.vn>
2021-05-01 22:05:18 +0700

    UI Shopping Bag

--- iBeauty_App/app/src/main/java/com/example/ibeauty_app/SignUpActivity.java
+++ iBeauty_App/app/src/main/java/com/example/ibeauty_app/SignUpActivity.java
@@ -22,7 +22,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-
 public class SignUpActivity extends AppCompatActivity {
 
     Button SignUpbtt;
